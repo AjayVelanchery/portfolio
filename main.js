4570a693bceafd7851a7d83e272d12194c55ba10
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-05-31 01:55:41 +0530

    media query

--- main.js
+++ main.js
@@ -19,3 +19,38 @@ function sendMail() {
     })
     .catch(err => console.log(err));
 }
+
+
+function toggleMenu(){
+    var menuToggle=document.querySelector('.toggle');
+    var menu=document.querySelector('.menu');
+  
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+}
+
+
+
+const sr=ScrollReveal({
+
+    origin:'top',
+    distance:'85px',
+    duration:2000,
+    reset:true
+})
+
+sr.reveal('.home-text',{})
+    sr.reveal('.homeimg',{delay:200})
+    sr.reveal('.social',{delay:200})
+
+
+sr.reveal('.about-img',{})
+sr.reveal('.heading',{})
+sr.reveal('.about-text',{delay:200})
+
+sr.reveal('.skill',{delay:200})
+sr.reveal('.project-content',{delay:200})
+
+sr.reveal('.contact-form',{delay:200})
+
+
